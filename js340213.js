b6f9920ffebaac24f32b45dc6d7c115d54c561bc
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-18 23:00:23 +0900

    24 .09 .18 Programmers Lv1 - PCCP 기출문제 동영상 재생기
    🔖 타임스탬프 공식 활용 / Math floor / forEach 활용
    
    ✏️ 주워진 시간을 타임스탬프처럼 1초당 1000으로 계산하여 치환하여 계산
    
    ❌ 현재 프로그래머스 1번과 10번에 반려됨.
    ✏️ 질문 사항에 게시됨. 리댓 대기중

---
+++ js340213.js
@@ -0,0 +1,42 @@
+const solution = (video_len, pos, op_start, op_end, commands) => {
+
+    // "prev" : 10초 전으로 이동
+    // "next" : 10초 후로  이동
+
+    let totalTime = setTimer(video_len);
+    let posTime = setTimer(pos);
+    let opStart = setTimer(op_start);
+    let opEnd = setTimer(op_end);
+
+    commands.forEach(commandStr => {
+        commandStr=="next" && nextAction();
+        commandStr=="prev" && prevAction();
+    });
+
+    let resultMM = Math.floor(posTime/60000);
+    let resultSS = Math.floor((posTime - (resultMM * 60000)) / 1000)
+
+    function setTimeLine(time){
+        return (""+time).length>1?""+time:"0"+time
+    }
+
+    return  setTimeLine(resultMM) + ":" + setTimeLine(resultSS);
+
+    function nextAction(){
+        posTime = ((posTime+10000)>=totalTime&&totalTime)||
+        ((posTime>=opStart&&posTime<=opEnd)&&opEnd+10000)||
+        posTime+10000;
+    }
+    
+    function prevAction(){
+        posTime = ((posTime-10000)>=0)&&posTime-10000||0;
+    }
+}
+
+const setTimer = (timeStr) => timeStr.split(":").map(data=>+data).reduce((result,num,index)=>(index==0&&result+(num*60000))||(index==1&&result+(num*1000)),0);
+
+
+
+// console.log(solution("34:33", "00:55", "00:55" ,"02:55",["prev","next","prev"]));
+console.log(solution("34:33", "00:05", "00:55" ,"02:55",["prev","prev","prev","next"]));
+// console.log(solution("34:33", "09:50", "10:00", "13:00", ["next", "next", "next", "prev"]));
