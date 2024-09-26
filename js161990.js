073887a78f224d525a9d2d036a76f8d3682ad4bc
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-26 21:15:38 +0900

    24 .09 .26 Programmers Lv1 - 바탕화면 정리
    🔖 Array / fill / map / flat / filter / Math min / max 활용
    
    ✏️ 문자열 리스트로 되어 있는 매개변수를 Array와 fill / map로 맵 데이터로 생성
    ✏️ 맵데이터로 생성하면서 '#'으로 표시된 파일 원소는 index값을 활용하여 좌표 데이터로 입력
    
    ✏️ flat 메소드를 활용하여 2차원 배열을 1차원 배열로 나열 후
    ✏️ filter로 좌표를 제외한 빈 파일 원소 제거
    ✏️ 이후 좌표 값들을 X와 Y로 분리 후
    ✏️ X 값 중 최소값과 최대값 + 1 / Y 값 중 최소값과 최대값 +1 산출
    ✏️ 산출된 데이터를  기반으로 배열로 나열 후 반환

--- js161990.js
+++ js161990.js
@@ -1,3 +1,18 @@
 const solution = (wallpaper) => {
     
+    const mapData = Array(wallpaper.length).fill().map((_,indexY)=>[...wallpaper[indexY]].map((data,indexX)=>data=="#"&&[indexY,indexX]||data)).flat().filter(data=>data!='.');
+    
+    const fileX = mapData.map(data=>data[0]);
+    const fileY = mapData.map(data=>data[1]);
+
+    let lux = Math.min(...fileX);
+    let luy = Math.min(...fileY);
+    let rdx = Math.max(...fileX)+1;
+    let rdy = Math.max(...fileY)+1;
+
+    return [lux,luy,rdx,rdy]
+
 }
+
+console.log(solution([".#...", "..#..", "...#."]));
+console.log(solution(["..........", ".....#....", "......##..", "...##.....", "....#....."]));
