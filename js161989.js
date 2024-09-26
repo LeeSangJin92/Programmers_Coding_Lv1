a2096ce296100bac7b63c4680944bdb34aa66bae
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-26 23:06:12 +0900

    24 .09 .26 Programmers Lv1 - 덧칠하기
    🔖 reduce / push / shift 활용
    
    ✏️ 색칠해야하는 영역 매개변수 section을 기반으로 reduce 활용
    ✏️ reduce 초기값을 이중 배열로 설정 후 원소값에 section의 첫번째 원소를 선출하여 입력.
    🛠️ 색칠하는 첫번째 구역은 section 중 첫번째 원소부터라서 shift 메소드로 가져와 입력해 놓음
    ✏️ result 배열 중 마지막의 첫번째 원소값에 색칠하는 롤러의 길이(m)만큼 더한 후 -1로 index로 계산하여 해당 범위에 색칠해야하는 영역 num이 포함될 경우 해당 배열에 추가
    ✏️ 만약 범위를 벗어나면 새로운 배열을 추가 ( num 포함된 배열)
    ✏️ 마지막에 1차 배열의 길이를 반환하면 횟수가 나옴

--- js161989.js
+++ js161989.js
@@ -1,3 +1,8 @@
-const solution = () => {
-    
-}
+const solution = (n, m, section) => 
+    section.reduce((result,num)=>{
+        result[result.length-1][0]+m-1>=num&&result[result.length-1].push(num)||
+        result.push([num]);
+        return result
+    },[[section.shift()]]).length;
+
+console.log(solution(8,4,[2,3,6]))
