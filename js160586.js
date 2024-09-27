f9986fffd9729b036d15fe33a50e512700517223
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-27 11:31:15 +0900

    24 .09 .27 Programmers Lv1 - 대충 만든 자판
    🔖 flat / new Set / map / reduce / Math min / indexOf / filter 활용
    
    ✏️ flat로 keymap를 1차원 배열로 생성
    ✏️생성된 1차원 배열을 new Set으로 중복 제거
    ✏️ 중복 제거된 원소 배열로 reduce 활용하여 객체로 생성
    ✏️ 객체는 중복제거된 원소가 key되고 keymap에서 indexOf로 가장 빠른 index추출
    ✏️ filter로 -1 제거
    ✏️ 이후 targets을 map으로 돌려서 얕은 복사를 활용하여 문자열을 원소로 나눈 후 각 keyList에서 해당 값을 누적합산함.
    ✏️ 만약 해당 값이 없을 경우 reduce는 -1을 반환

--- js160586.js
+++ js160586.js
@@ -1,7 +1,12 @@
 const solution = (keymap, targets) => {
     let wordList = [...new Set(keymap.map(str=>[...str]).flat())];
+    let keyList = wordList.reduce((result,word)=>{
+        return {...result,[word]:Math.min(...keymap.map(str=>[...str].indexOf(word)).filter(data=>data!=-1))+1}
+        },{});
     
-    return wordList;
+    return targets.map(str=>[...str].reduce((result,word)=> (result>-1&&Object.keys(keyList).includes(word)&&result+keyList[word])||-1 ,0))
 }
 
 console.log(solution(["ABACD", "BCEFD"], ["ABCD","AABB"]))
+console.log(solution(["AA"], ["B"]))
+console.log(solution(["AGZ", "BSSS"], ["ASA","BGZJ"]))
