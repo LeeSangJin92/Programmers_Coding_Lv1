b34517fd632182665d3264c7c02563b38cceb3e0
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-10-02 00:45:58 +0900

    24 .10 .02 Programmers Lv1 - 명예의 전당 (1)
    🔖 reduce / push / sort / length 활용
    
    ✏️ 명예의 전당 리스트 먼저 생성
    ✏️ 점수 리스트를 reduce로 활용
    ✏️ 점수를 우선 명예의 전당에 추가
    ✏️ 추가된 점수를 포함한 명예의 전당 리스트 sort로 정렬
    ✏️ 명예의 전당 인원 초과 시 마지막 낙오자 pop로 제거
    ✏️ 명예의 전당 중 마지막 번호를 result에 push
    ✏️ result를 최종적으로 반환

--- js138477.js
+++ js138477.js
@@ -1,1 +1,25 @@
-const solution = (k, score) => {};
+const solution = (k, score) =>{
+    // 명예의 전당 리스트
+    let rankList = [];
+    return score.reduce((result, number, index)=>{
+        
+        // 명예의 전당에 점수 추가
+        rankList.push(number);
+
+        // 명예의 전당 순의 순서 정렬
+        rankList.sort((a,b)=>b-a);
+
+        // 명예의 전당에서 낙오된 점수 제거
+        rankList.length>k&&rankList.pop();
+
+        // 명예의 전당 마지막 점수 결과 리스트에 추가
+        result.push(rankList[rankList.length-1])
+
+        // 결과 반환
+        return result;
+    },[]);
+}
+
+
+console.log(solution(3, [10, 100, 20, 150, 1, 100, 200]));
+console.log(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]));
