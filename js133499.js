71725086260c7145d521ad067a3518b216d3d45f
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-10-04 15:52:21 +0900

    24 .10 .04 Programmers Lv1 - 옹알이 (2) ✅ 해결
    🔖 reduce / 정규표현식 / test / replace / length / 비교연산자 활용
    
    ✏️ 문자열 배열을 redcue로 검사하면 서 결과 반환
    ✏️ 우선 연속 발음을 정규식으로 검사
    ✏️ 연속 발음이 없는 상태에서 발음이 존재 검사
    ✏️ 있다면 replace로 해당 발음 문자열에서 제거
    ✏️ 모든 발음을 제거 후 남은 문자열이 없을 때 result 증가
    ✏️ 문자열이 남아 있다면 result 유지

--- js133499.js
+++ js133499.js
@@ -1,12 +1,26 @@
 const solution = (babbling) => babbling.reduce((result, str)=>
-    (str.replace(/aya/," ")
-    .replace(/ye/," ")
-    .replace(/woo/," ")
-    .replace(/ma/," ")
-    .replace(/\s/g,"")
-    .length==0&&++result)||result
-    ,0)
+    {
+        // 연속 발음 검사
+        if(!/ayaaya|yeye|woowoo|mama/g.test(str))
+        
+        // 발음 제거
+        if(/aya|ye|woo|ma/g.test(str)){
+            str = str.replace(/aya/g," ")
+            .replace(/ye/g," ")
+            .replace(/woo/g," ")
+            .replace(/ma/g," ")
+            .replace(/\s/g,"");
+        }
 
-console.log(solution(["aya", "yee", "u", "maa"]));
-console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	));
-console.log(solution(["yeayaye"]));
+        // 남은 발음 확인
+        result = str.length==0&&++result||result
+        return result;
+    },0)
+    
+    
+
+// console.log(solution(["aya", "yee", "u", "maa"]));
+// console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	));
+// console.log(solution(["ayayeayaaya"]));
+console.log(solution(["ayayemaaya"]));
+console.log(solution(["mayayemaayaa"]));
